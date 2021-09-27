74c36bfcf31de432f11c1a3e02eb33e01c23ae8c
Aksr aashish <76800327+aksr-aashish@users.noreply.github.com>
2021-09-27 05:55:16 -0700

    Update pluginhelpers.py
    minor change by axel

--- MizuharaSmexyBot/functions/pluginhelpers.py
+++ MizuharaSmexyBot/functions/pluginhelpers.py
@@ -11,8 +11,9 @@ from PIL import Image
 from pyrogram import Client
 from pyrogram.errors import FloodWait, MessageNotModified
 from pyrogram.types import Chat, Message, User
-from vexana import OWNER_ID, GBAN_LOGS
-from vexana.pyrogramee.pyrogram import pbot
+from MizuharaSmexyBot
+import OWNER_ID, GBAN_LOGS
+from MizuharaSmexyBot.pyrogramee.pyrogram import pbot
 
 
 def get_user(message: Message, text: str) -> [int, str, None]:
